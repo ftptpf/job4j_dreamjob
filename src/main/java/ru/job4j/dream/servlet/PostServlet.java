d9ba0bb14cff3a0584a20d77bc54ac59aa2fd835
ftptpf <inboxru@yandex.ru>
2022-01-24 09:23:37 +0300

    0. MVC в Servlet. [#6868]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -3,6 +3,7 @@ package ru.job4j.dream.servlet;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +12,18 @@ import java.io.IOException;
 public class PostServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("posts", Store.instOf().findAllPosts());
+        req.getRequestDispatcher("posts.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().save(
                 new Post(
                         Integer.parseInt(req.getParameter("id")), req.getParameter("name")
                 ));
-        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+        resp.sendRedirect(req.getContextPath() + "/posts.do");
     }
 }
