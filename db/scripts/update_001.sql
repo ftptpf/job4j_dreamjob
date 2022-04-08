50c8025823eb18a064eaf4a1a07be5e7f5770058
ftptpf <inboxru@yandex.ru>
2022-04-08 09:52:24 +0300

    5. Реализация интерфейса [#2519] - tests

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,21 +1,20 @@
+CREATE TABLE IF NOT EXISTS city (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
 CREATE TABLE IF NOT EXISTS post (
     id SERIAL PRIMARY KEY,
     name TEXT,
     create_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS candidate (
+CREATE TABLE candidate (
     id SERIAL PRIMARY KEY,
     name TEXT,
     city_id INT REFERENCES city (id),
     create_date DATE
 );
-
-CREATE TABLE IF NOT EXISTS city (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
-
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
--- src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -3,6 +3,8 @@ package ru.job4j.dream.servlet;
 import org.junit.Test;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.DbStore;
+import ru.job4j.dream.store.MemStore;
+import ru.job4j.dream.store.Store;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,10 +20,12 @@ public class CandidateServletTest {
 
     @Test
     public void whenCreateCandidate() throws IOException {
+        //DbStore store = mock(DbStore.class);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         when(req.getParameter("id")).thenReturn("0");
         when(req.getParameter("name")).thenReturn("new candidate");
+        when(req.getParameter("citiesId")).thenReturn("1");
         new CandidateServlet().doPost(req, resp);
         Candidate candidate = DbStore.instOf().findByNameCandidate("new candidate");
         assertThat(candidate, notNullValue());
